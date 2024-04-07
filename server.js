841e603a0ee33757b1e5fda29ca6be2aeffcb020
Dario Jularic <dariojularic0@gmail.com>
2024-04-07 13:48:06 +0200

    updated nodejs and mongoose

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").parse()
+}
+
 const express = require("express")
 const app = express()
 const expressLayouts = require("express-ejs-layouts")
 
 const indexRouter = require("./routes/index")
 
+// const dotenv = require("dotenv");
+
+// dotenv.config();
 
 app.set("view engine", "ejs")
 app.set("views", __dirname + "/views")
@@ -12,10 +19,11 @@ app.use(expressLayouts)
 app.use(express.static("public"))
 
 const mongoose = require("mongoose")
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on("error", error => console.error(error))
 db.once("open", error => console.log("Connected to Mongoose"))
+
 
 
 app.use("/", indexRouter)
